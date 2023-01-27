85868f98b239e61b3eb018785f7c81f27f775037
OmriHarary <omriharary@hotmail.com>
2023-01-27 13:50:55 -0500

    Set up as Spring Boot app
    - Generated with Spring Initializer
    - Included Kafka dependencies

---
+++ src/main/java/io/github/aaejo/manualintervention/ManualInterventionApplication.java
@@ -0,0 +1,13 @@
+package io.github.aaejo.manualintervention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualInterventionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualInterventionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aaejo/manualintervention/ManualInterventionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aaejo.manualintervention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManualInterventionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
