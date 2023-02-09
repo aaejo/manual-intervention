df5cd7f029a758d4a06a8d9ba237c506c702c50b
OmriHarary <omriharary@hotmail.com>
2023-02-09 12:21:14 -0500

    Kafka framework implementation
    - Introduced Kafka producer component using common record types for serialization
    - Added basic topic configuration, can be further configured if needed
    - Added Kafka consumer component which will be primary execution entrypoint

---
+++ src/main/java/io/github/aaejo/manualintervention/messaging/consumer/ManualInterventionListener.java
@@ -0,0 +1,19 @@
+package io.github.aaejo.manualintervention.messaging.consumer;
+
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import io.github.aaejo.messaging.records.IncompleteScrape;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@KafkaListener(id = "manual-intervention", topics = "manual-intervention")
+public class ManualInterventionListener {
+
+    @KafkaHandler
+    public void handle(IncompleteScrape incompleteScrape) {
+        log.info(incompleteScrape.toString());
+    }
+}
